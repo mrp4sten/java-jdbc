8d671be4bd7883aac45dc7222ea226a907ff55b5
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-08-20 17:05:32 -0600

    Closing conections try-with-resources

--- src/main/java/com/mrp4sten/org/ConnectionExample.java
+++ src/main/java/com/mrp4sten/org/ConnectionExample.java
@@ -2,9 +2,9 @@ package com.mrp4sten.org;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class ConnectionExample {
     public static void main(String[] args) {
@@ -14,18 +14,21 @@ public class ConnectionExample {
         String password = System.getenv("JDBC_PASSWORD");
 
         try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM products");
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                String query = "SELECT * FROM products";
+                try (PreparedStatement statement = connection.prepareStatement(query);
+                        ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        System.out.print(resultSet.getString("id"));
+                        System.out.print(" | ");
+                        System.out.print(resultSet.getString("name"));
+                        System.out.print(" | ");
+                        System.out.print(resultSet.getDouble("price"));
+                        System.out.print(" | ");
+                        System.out.println(resultSet.getDate("record_date"));
+                    }
+                }
             }
-
-            resultSet.close();
-            statement.close();
-            connection.close();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
