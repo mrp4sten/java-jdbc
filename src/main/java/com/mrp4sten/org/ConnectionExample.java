10eeafb2c841c8bcd63d1c7dd03dcd830c600753
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-08-23 17:58:58 -0600

    Updating ConnectionExample
    Adding Slf4j dependency

--- src/main/java/com/mrp4sten/org/ConnectionExample.java
+++ src/main/java/com/mrp4sten/org/ConnectionExample.java
@@ -1,39 +1,63 @@
 package com.mrp4sten.org;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mrp4sten.org.model.Category;
 import com.mrp4sten.org.model.Product;
 import com.mrp4sten.org.repository.ProductRepositoryImpl;
 import com.mrp4sten.org.repository.Repository;
+import com.mrp4sten.org.util.DBConnection;
 
 public class ConnectionExample {
-        public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Logger logger = LoggerFactory.getLogger(ConnectionExample.class);
+        try (Connection connection = DBConnection.getConnection()) {
 
-                System.out.println("=== PRODUCT LIST ===");
-                Repository<Product> repository = new ProductRepositoryImpl();
-                repository.list().forEach(System.out::println);
+            if (connection.getAutoCommit()) {
+                connection.setAutoCommit(false);
+            }
 
-                System.out.println("=== PRODUCT WITH ID 2 ===");
-                System.out.println(repository.byId(2L));
+            try {
 
-                System.out.println("=== ADD PRODUCT ===");
+                logger.info("=== PRODUCT LIST ===");
+                Repository<Product> repository = new ProductRepositoryImpl(connection);
+                repository.list().forEach(p -> logger.info(p.toString()));
+
+                logger.info("=== PRODUCT WITH ID 2 ===");
+                logger.info("Product: {}", repository.byId(2L));
+
+                logger.info("=== ADD PRODUCT ===");
                 java.util.Date date = new java.util.Date();
                 Category category = new Category();
                 category.setId(1L);
                 Product product = new Product(null, "Samsung s23 PRO", 16000.00, new Date(date.getTime()), category,
-                                "abc5678def");
+                        "abc5678def");
                 repository.save(product);
-                repository.list().forEach(System.out::println);
+                repository.list().forEach(p -> logger.info(p.toString()));
 
-                System.out.println("=== EDIT PRODUCT ===");
+                logger.info("=== EDIT PRODUCT ===");
                 Product updateProduct = new Product(4L, "Samsung Galaxy s23 PRO", 16000.00,
-                                null, category, "abc1234def");
+                        null, category, "abc1234def");
                 repository.save(updateProduct);
-                repository.list().forEach(System.out::println);
+                repository.list().forEach(p -> logger.info(p.toString()));
 
-                System.out.println("=== REMOVE PRODUCT ===");
+                logger.info("=== REMOVE PRODUCT ===");
                 repository.remove(4L);
-                repository.list().forEach(System.out::println);
+                repository.list().forEach(p -> logger.info(p.toString()));
+
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+
+            }
+
         }
+
+    }
 }
